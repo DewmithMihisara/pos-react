b892cc5d7084f0446310f6f4bf250fa055a7cd43
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-29 12:42:15 +0530

    import some

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ProtectedRouts from './util/ProtectedRouts';
+import SignIn from './pages/auth/SignIn';
+import SignUp from './pages/auth/SignUp';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <></>
+      <BrowserRouter>
+          <Routes>
+            <Route element={<ProtectedRouts />}>
+                <Route index element={<Home />}/>
+            </Route>
 
-    
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
