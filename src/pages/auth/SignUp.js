892455dff91b835c1b403fee1e26407ed6e488b0
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-03 08:32:21 +0530

    test

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -38,6 +38,7 @@ const SignUp=()=>{
       <div className="text-center mb-5">
         <h1 className="reg-topic topic">Sign-Up</h1>
         <p>Hey enter your details for create new account</p>
+        <p>test</p>
       </div>
       <form onSubmit={handleRegister}>
         <div className="form-group mb-4">
